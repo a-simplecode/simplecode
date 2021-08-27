28fe9cc3ee2efa92039b1b181836786179cf4c81
aminects <amineamine19961996@gmail.com>
2021-08-27 21:47:16 +0300

    FIX: structure FIX: CSS structure ADD: background color and footer border (not sure if will stay)

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
 import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
-export default function Layout(props){
-    return(
-        <div>
-            <Header/>
-            <div className={styles.body}>
-            {props.children}
-            </div>
-        </div>
-    )
+export default function Layout(props) {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.body}>{props.children}</div>
+      <Footer />
+    </div>
+  );
 }
