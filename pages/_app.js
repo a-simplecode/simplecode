3cb5f6380cc971ed76bc4e9748e48c3afeb15c2d
Amine Amine <amineamine19961996@gmail.com>
2021-09-16 22:46:26 +0300

    ADD: ico for the website

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="I'am a full stack developer specialized in Web Development as Next JS (Pre-rendering), React JS (Hooks and context), JavaScript, Redux, HTML, CSS. I also write the simplest code possible, fully SEO and code reusability with high performance for having the best websites."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/amine.ico" />
       </Head>
       <Layout>
         {loading ? (
