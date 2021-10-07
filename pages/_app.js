c763ae8cba8896dc73f9b2b77a9814c60a24a064
Amine Amine <amineamine19961996@gmail.com>
2021-10-07 22:36:56 +0300

    FIX: ico

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="I'am a full stack developer specialized in Web Development as Next JS (Pre-rendering), React JS (Hooks and context), JavaScript, Redux, HTML, CSS. I also write the simplest code possible, fully SEO and code reusability with high performance for having the best websites."
         />
-        <link rel="icon" href="/images/simpleCode.ico" />
+        <link rel="icon" href="/images/simpleCode2.ico" />
       </Head>
       <Layout>
         {loading ? (
