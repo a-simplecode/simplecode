379f8b604c10331c135bc496cee72790ccc7c7eb
Amine Amine <amineamine19961996@gmail.com>
2021-10-27 18:04:10 +0300

    FIX: PWA final version

--- next.config.js
+++ next.config.js
@@ -10,5 +10,4 @@ module.exports = withPWA({
       "packagequality.com"
     ]
   },
-  reactStrictMode: true,
 });
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-const APP_NAME = 'simplecode'
-export default class extends Document {
-  
+const APP_NAME = "simplecode";
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    return await Document.getInitialProps(ctx)
+    return await Document.getInitialProps(ctx);
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en' dir='ltr'>
         <Head>
         <meta name='application-name' content={APP_NAME} />
           <meta name='apple-mobile-web-app-capable' content='yes' />
@@ -17,10 +16,23 @@ export default class extends Document {
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name='theme-color' content='#FFFFFF' />
-          
+          <link rel='apple-touch-icon' sizes='180x180' href='/images/simpleCode.png' />
           <link rel='manifest' href='/manifest.json' />
           <link rel='shortcut icon' href='/images/simpleCode2.ico' />
-          </Head>
+          <style>{
+            `
+            html, body, #__next {
+              height: 100%;
+            }
+            #__next {
+              margin: 0 auto;
+            }
+            h1 {
+              text-align: center;
+            }
+            `
+          }</style>
+        </Head>
         <body>
           <Main />
           <NextScript />
